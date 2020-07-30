16fd57e8e22211bf47a742f699530e7f2c3127bd
Kian Molani <molanikian@gmail.com>
2020-07-30 01:20:39 -0400

    Updates

---
+++ precache-manifest.f36611a75e585b587b5bf5574808b51a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3326da282a965c83c904fc64121a3fdc",
+    "url": "/decentralized-video-streaming-app/index.html"
+  },
+  {
+    "revision": "c32715e3730cbd6eb34b",
+    "url": "/decentralized-video-streaming-app/static/css/2.9e56c109.chunk.css"
+  },
+  {
+    "revision": "8a4160b679821c74c626",
+    "url": "/decentralized-video-streaming-app/static/css/main.9006c98d.chunk.css"
+  },
+  {
+    "revision": "c32715e3730cbd6eb34b",
+    "url": "/decentralized-video-streaming-app/static/js/2.ea0a76c8.chunk.js"
+  },
+  {
+    "revision": "bffda368d73ae2e202d93894f7a3c570",
+    "url": "/decentralized-video-streaming-app/static/js/2.ea0a76c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4160b679821c74c626",
+    "url": "/decentralized-video-streaming-app/static/js/main.74a7f6f6.chunk.js"
+  },
+  {
+    "revision": "ec5deed565a582e413d0",
+    "url": "/decentralized-video-streaming-app/static/js/runtime-main.9e784ec3.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/decentralized-video-streaming-app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/decentralized-video-streaming-app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/decentralized-video-streaming-app/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/decentralized-video-streaming-app/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/decentralized-video-streaming-app/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/decentralized-video-streaming-app/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/decentralized-video-streaming-app/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/decentralized-video-streaming-app/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/decentralized-video-streaming-app/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/decentralized-video-streaming-app/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/decentralized-video-streaming-app/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/decentralized-video-streaming-app/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/decentralized-video-streaming-app/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/decentralized-video-streaming-app/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/decentralized-video-streaming-app/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/decentralized-video-streaming-app/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/decentralized-video-streaming-app/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "2b470564821c19dc383cf82898a868e4",
+    "url": "/decentralized-video-streaming-app/static/media/logo.2b470564.svg"
+  }
+]);
